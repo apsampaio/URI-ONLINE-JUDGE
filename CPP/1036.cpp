c90936289c9ca3fb88e126964a909a50ca7c3475
apsampaio <andre03.sampaio@gmail.com>
2021-09-17 22:25:28 -0300

    add 1036

--- CPP/1036.cpp
+++ CPP/1036.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
- 
+#include <math.h>
+
 using namespace std;
- 
-int main() {
- 
-    /**
-     * Escreva a sua solução aqui
-     * Code your solution here
-     * Escriba su solución aquí
-     */
- 
+
+int main()
+{
+    float A, B, C;
+
+    cin >> A >> B >> C;
+
+    float delta = (pow(B, 2)) - (4 * A * C);
+
+    float R1 = (-B + sqrt(delta)) / (2 * A);
+    float R2 = (-B - sqrt(delta)) / (2 * A);
+
+    if (isnan(R1) || isnan(R2)) {
+        cout << "Impossivel calcular\n";
+    } else {
+        std::cout.precision(5);
+        cout << std::fixed;
+        cout << "R1 = " << R1 << "\n";
+        cout << "R2 = " << R2 << "\n";
+    }
+
     return 0;
 }
